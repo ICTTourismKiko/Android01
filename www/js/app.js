68173a9ad48d3e78c1b7d405f730244129fb1bd3
ToshikiIkeda <g2117004@fun.ac.jp>
2017-05-30 20:48:29 +0900

    マップ画面まで遷移できるようになった。（マップは表示されない）

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,11 @@
 function Pop_to_list() {
     document.getElementById("navi").pushPage("list.html"); 
 }
+
+function Pop_to_Map() {
+    document.getElementById("navi").pushPage("map.html"); 
+}
+
+function Pop_to_camera() {
+    document.getElementById("navi").pushPage("camera.html"); 
+}
