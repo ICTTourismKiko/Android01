d9ad2be76ee543814a8630892648fcfdd975d314
ToshikiIkeda <g2117004@fun.ac.jp>
2017-05-31 19:51:20 +0900

    cssファイルを分けた

--- www/js/map.js
+++ www/js/map.js
@@ -6,7 +6,6 @@ document.addEventListener('init', function(event) {
             zoom: 14,                                                 //地図の倍率
             mapTypeId: google.maps.MapTypeId.ROADMAP                  //地図の種類
         };
-            
         var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
     }
 });
