914a1b8788fcfef3bd9744a0eca6f97fe694b5a7
ToshikiIkeda <g2117004@fun.ac.jp>
2017-05-31 16:27:41 +0900

    リストに表示されるカードを増やした

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,15 @@
-function Pop_to_list() {
+function Push_to_list() {
     document.getElementById("navi").pushPage("list.html"); 
 }
 
-function Pop_to_Map() {
+function Push_to_Map() {
     document.getElementById("navi").pushPage("map.html");
 }
 
-function Pop_to_camera() {
+function Push_to_camera() {
     document.getElementById("navi").pushPage("camera.html");
 }
 
-function Pop_to_edit() {
+function Push_to_edit() {
     document.getElementById("navi").pushPage("edit.html");
 }
