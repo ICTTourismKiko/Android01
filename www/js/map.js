148270021071fbe27bda56ef947f051b58dd841d
ToshikiIkeda <g2117004@fun.ac.jp>
2017-05-31 19:44:38 +0900

    木古内の座標を設定

--- www/js/map.js
+++ www/js/map.js
@@ -2,7 +2,7 @@ document.addEventListener('init', function(event) {
     var page = event.target;
     if(page.id == "map") {
         var mapOptions = {
-            center: new google.maps.LatLng(35.658892, 139.755286),    //地図上で表示させる緯度経度
+            center: new google.maps.LatLng(41.677503, 140.435202),    //地図上で表示させる緯度経度
             zoom: 14,                                                 //地図の倍率
             mapTypeId: google.maps.MapTypeId.ROADMAP                  //地図の種類
         };
