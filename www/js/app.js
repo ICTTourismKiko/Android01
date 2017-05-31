3f8c84e9b4f28ff3a52f0ced263259bed864191a
ToshikiIkeda <g2117004@fun.ac.jp>
2017-05-31 16:18:02 +0900

    マップが表示されるようになりました

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,13 @@ function Pop_to_list() {
 }
 
 function Pop_to_Map() {
-    document.getElementById("navi").pushPage("map.html"); 
+    document.getElementById("navi").pushPage("map.html");
 }
 
 function Pop_to_camera() {
-    document.getElementById("navi").pushPage("camera.html"); 
+    document.getElementById("navi").pushPage("camera.html");
+}
+
+function Pop_to_edit() {
+    document.getElementById("navi").pushPage("edit.html");
 }
---
+++ www/js/map.js
@@ -0,0 +1,12 @@
+document.addEventListener('init', function(event) {
+    var page = event.target;
+    if(page.id == "map") {
+        var mapOptions = {
+            center: new google.maps.LatLng(35.658892, 139.755286),    //地図上で表示させる緯度経度
+            zoom: 14,                                                 //地図の倍率
+            mapTypeId: google.maps.MapTypeId.ROADMAP                  //地図の種類
+        };
+            
+        var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+    }
+});
